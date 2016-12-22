9cd6f4318ef545e8fbfe11d5fb9ff2ad31995ecc
Nathan Fain <cyphunk@gmail.com>
2016-12-21 21:15:31 -0500

    .

--- public/js/app.js
+++ public/js/app.js
@@ -459,6 +459,8 @@ var Voter = _react2['default'].createClass({
                     //                   function(v){
                     //                        return v == signal_key;
                     //                   }).length;
+                    debug_log('Voter render - signals', signals);
+                    debug_log('Voter render - signals[signal_key]', signals[signal_key]);
                     debug_log('Voter render - vote_count', signals[signal_key].vote_count);
 
                     return _react2['default'].createElement(Signal, {
