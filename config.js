9ded66f4d632bbb8ebb7e70a577d9ff5bd0f1cd1
Nathan Fain <cyphunk@gmail.com>
2016-12-26 08:44:40 -0500

    .

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ config.epoch.pause_forced = false  // when true client interface fade out all bu
 // config.epoch.pause_show_progress = true  // show progress cont down
 config.epoch.start_new_epoch_after_pause = false  // if false forces admin bang.
 config.epoch.winner_switches_to_write_tab = true  // if true then whoever wens an epoch will be switched to the writer tab in their ui
-config.epoch.delete_winner = false
+config.epoch.delete_winner = true
 
 
 module.exports = config
