1770c8619b4c656826fc1283b1037b19c0abe9ee
Nathan Fain <cyphunk@gmail.com>
2017-03-11 19:20:55 -0500

    .

--- client/app.jsx
+++ client/app.jsx
@@ -80,6 +80,16 @@ var Writer = React.createClass({
             user : this.props.user,
             text : this.props.signal //.replace(/\n|\./g,'')
         });
+        // thank you message
+        var message = "submitted<br><br><div style='font-size:0.8em'>you can change your message at any time</div>"
+        if (broadcast_message_timer)
+            window.clearTimeout(broadcast_message_timer)
+        var elem = document.getElementById('broadcast_message')
+        elem.innerHTML = message;
+        elem.style.display = 'flex'
+        broadcast_message_timer = window.setTimeout(function(){
+            document.getElementById('broadcast_message').style.display='none'
+        }, 6000)
     },
     handle_change(e) {
         debug_log('Writer handle_change - match .', e.target.value.match(/\. *$/g))
@@ -100,21 +110,11 @@ var Writer = React.createClass({
 
         if (submit) {
             debug_log("Writer handle_change - to stage: "+e.target.value+' code:'+e.keyCode);
-            socket.emit('send:signal', {
-                user : this.props.user,
-                text : e.target.value
-            });
-            // thank you message
-            var message = "submitted<br><br><div style='font-size:0.8em'>you can change your message at any time</div>"
-            if (broadcast_message_timer)
-                window.clearTimeout(broadcast_message_timer)
-            var elem = document.getElementById('broadcast_message')
-            elem.innerHTML = message;
-            elem.style.display = 'flex'
-            broadcast_message_timer = window.setTimeout(function(){
-                document.getElementById('broadcast_message').style.display='none'
-            }, 4000)
-
+            this.handle_submit(e)
+            // socket.emit('send:signal', {
+            //     user : this.props.user,
+            //     text : e.target.value
+            // });
         }
         //Supposedly pressing enter in text input calls the forms submit
         // so we do not need to call this.handle_submit on enter
